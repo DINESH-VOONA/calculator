152a82413782aefdbeb4c6f1fa73f47ce840c6ff
Voona Dinesh <voonadinesh3@gmail.com>
2025-08-13 15:45:40 +0530

    second update

--- index.js
+++ index.js
@@ -7,21 +7,25 @@ function display(val){
     input.value += val;
 }
 
+//! for AC Button
 let AC = document.getElementById("AC");
 AC.addEventListener("click",()=>{
     input.value = "";
 })
 
+//! for clear Button
 let clear = document.getElementById("clear")
 clear.addEventListener("click",()=>{
     input.value = input.value.slice(0,-1);
 })
 
+//! for res (=) Button
 let res = document.getElementById("res");
 res.addEventListener("click",()=>{
     input.value = eval(input.value);
 })
 
+//! for 0 button
 let num0 = document.getElementById("num0");
 num0.addEventListener("click",()=>{
     
@@ -32,6 +36,82 @@ num0.addEventListener("click",()=>{
     else
     {
         input.value +='0';
+    }  
+})
+//! for (+) button
+let btnplus = document.getElementById("plus");
+btnplus.addEventListener("click",()=>{
+    if(input.value.charAt(input.value.length-1) == '+')
+    {
+        input.value.charAt(input.value.length-1) == '+';
     }
-    
+    else
+    {
+        input.value += '+';
+    }
+})
+
+//! for (-) button
+let btnminus = document.getElementById("minus");
+btnminus.addEventListener("click",()=>{
+    if(input.value.charAt(input.value.length-1) == '-')
+    {
+        input.value.charAt(input.value.length-1) == '-';
+    }
+    else
+    {
+        input.value += '-';
+    }
+})
+
+//! for (*) button
+let btnmul = document.getElementById("mul");
+btnmul.addEventListener("click",()=>{
+    if(input.value.charAt(input.value.length-1) == '*')
+    {
+        input.value.charAt(input.value.length-1) == '*';
+    }
+    else
+    {
+        input.value += '*';
+    }
+})
+
+//! for (/) button
+let btndiv = document.getElementById("div");
+btndiv.addEventListener("click",()=>{
+    if(input.value.charAt(input.value.length-1) == '/')
+    {
+        input.value.charAt(input.value.length-1) == '/';
+    }
+    else
+    {
+        input.value += '/';
+    }
+})
+
+//! for (%) button
+let btnmod = document.getElementById("mod");
+btnmod.addEventListener("click",()=>{
+    if(input.value.charAt(input.value.length-1) == '%')
+    {
+        input.value.charAt(input.value.length-1) == '%';
+    }
+    else
+    {
+        input.value += '%';
+    }
+})
+
+//! for (.) button
+let btndot = document.getElementById("dot");
+btndot.addEventListener("click",()=>{
+    if(input.value.charAt(input.value.length-1) == '.')
+    {
+        input.value.charAt(input.value.length-1) == '.';
+    }
+    else
+    {
+        input.value += '.';
+    }
 })
